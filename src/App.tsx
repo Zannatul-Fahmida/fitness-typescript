3336f29830e943e5f353e73c8c2299fdd94c7c46
zannatul <jannatulfahmida15@gmail.com>
2023-11-07 21:22:27 +0600

    benefits section added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import Navbar from '@/components/shared/Navbar'
 import { SelectedPage } from '@/shared/types';
 import Home from '@/components/home/Home';
+import Benefits from './components/home/Benefits';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
@@ -13,7 +14,7 @@ function App() {
       setIsTopOfPage(true)
       setSelectedPage(SelectedPage.Home)
     }
-    if(window.screenY!==0) setIsTopOfPage(false);
+    if(window.scrollY!==0) setIsTopOfPage(false);
   }
   window.addEventListener("scroll", handleScroll);
   return () => window.removeEventListener("scroll", handleScroll)
@@ -23,6 +24,7 @@ function App() {
   return <div className='app bg-gray-20'>
     <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     <Home setSelectedPage={setSelectedPage} />
+    <Benefits setSelectedPage={setSelectedPage} />
   </div>
 }
 
